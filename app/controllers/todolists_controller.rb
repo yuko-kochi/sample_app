c05a34e6032acb62d701c28695d197c3abf76a6e
EC2 Default User <ec2-user@ip-172-31-59-65.ec2.internal>
2021-06-05 06:57:37 +0000

    [Finish]2-4

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,18 @@
+class TodolistsController < ApplicationController
+  
+  def new
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+  
+  private
+  def list_params
+    params.require(:list).pemit(:title, :body)
+  end
+  
+end
